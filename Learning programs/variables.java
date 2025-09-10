6d4b2ee509b3e1fa9a624f9abcea3f8f1642c845
Vincent Tirimba <vincent.tirimba05@gmail.com>
2025-09-11 00:02:47 +0300

    Added a new learning program

---
+++ Learning programs/variables.java
@@ -0,0 +1,24 @@
+
+    public class variables
+    {
+        public static void main(String a[])
+        {
+            int num1 = 3;
+            int num2 = 5;
+
+            byte bt = 54;
+            int num = 45677;
+            short sh = 56;
+            long ln = 4567l;
+
+            float fl = 56.6f;
+            double dl = 56.7;
+
+            char c = 'k';
+
+            boolean bl = true;
+
+            int sum = num1 + num2;
+            System.out.println(sum);
+        }
+    }
