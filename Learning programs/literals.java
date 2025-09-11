e7efcb2929ef6f15580a8a1e05660e47679077ca
Vincent Tirimba <vincent.tirimba05@gmail.com>
2025-09-11 19:52:17 +0300

    Added a new learning program

---
+++ Learning programs/literals.java
@@ -0,0 +1,18 @@
+
+class literals
+{
+    public static void main(String args[])
+    {
+        byte b = 124;
+        int k = 257;
+
+        b = (byte)k;
+
+        double f = 5.6;
+
+        int v = (int) f;
+
+        System.out.println(b);
+        System.out.println(v);
+    }
+}
