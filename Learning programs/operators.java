17d229e7f48af3d9604fb52925a789072a8ac280
Vincent Tirimba <vincent.tirimba05@gmail.com>
2025-09-12 00:20:54 +0300

    Added a new learning program

---
+++ Learning programs/operators.java
@@ -0,0 +1,22 @@
+
+
+class operators 
+{
+    public static void main (String args [])
+    {
+        int num1 = 7;
+        int num2 = 5;
+
+        int sum = num1 + num2;
+        int dif = num1 - num2;
+        int prod = num1 * num2;
+        int div = num1 / num2;
+        int mod = num1 % num2;
+
+        System.out.println(sum);
+        System.out.println(dif);
+        System.out.println(prod);
+        System.out.println(div);
+        System.out.println(mod);
+    }    
+}
