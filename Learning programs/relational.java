4718f945df5a53bf7452991d0669922ce14ac258
Vincent Tirimba <vincent.tirimba05@gmail.com>
2025-09-12 00:53:56 +0300

    Added a new learning program

---
+++ Learning programs/relational.java
@@ -0,0 +1,20 @@
+
+public class relational 
+{
+    public static void main (String args[])
+    {
+        int x = 6;
+        int y = 5;
+
+        boolean v = x < y;
+
+        // x > y;
+        // x <= y;
+        // x >= y;
+        // x == y;
+        // x != y;
+        
+
+        System.out.println(v);
+    }    
+}
