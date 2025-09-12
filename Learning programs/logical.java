2f167bc5a0afe0800727402239d6955ee7a53d56
Vincent Tirimba <vincent.tirimba05@gmail.com>
2025-09-13 00:05:08 +0300

    Added a new learning program

---
+++ Learning programs/logical.java
@@ -0,0 +1,16 @@
+public class logical 
+{
+    public static void main(String args[])
+    {
+        int x = 4;
+        int y = 6;
+
+        double d1 = 5.4;
+        double d2 = 4.3;
+
+        boolean v = x > y && d1 > d2;
+        //boolean v = x > y || d1 > d2;
+
+        System.out.println(v);
+    }
+}
