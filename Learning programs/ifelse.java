4bdceb3a46d9f0ab927b3cd7ea766bcf18cf4e21
Vincent Tirimba <vincent.tirimba05@gmail.com>
2025-09-14 01:28:26 +0300

    Added a new learning program

---
+++ Learning programs/ifelse.java
@@ -0,0 +1,26 @@
+public class ifelse 
+{
+    public static void main(String args[])
+    {
+        int x = 8;
+
+        if (x > 10 && x <= 20)
+            System.out.println("Hello!");
+        else
+            System.out.println("Bye!");
+
+        int a = 7;
+        int b = 9;
+
+        if (a > b)
+        {
+            System.out.print(a);
+            System.out.print(" is greater than 9");
+        }
+        else
+        {
+            System.out.print(b);
+            System.out.print(" is greater than 7");
+        }
+    }    
+}
