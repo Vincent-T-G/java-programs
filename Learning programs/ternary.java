d76b60bc7d6a4e937cbbfa1107f494c689fd1f79
Vincent Tirimba <vincent.tirimba05@gmail.com>
2025-09-15 23:45:25 +0300

    Added a new learning program

---
+++ Learning programs/ternary.java
@@ -0,0 +1,22 @@
+public class ternary 
+{
+    public static void main(String args[])
+    {
+        int x = 5;
+        int result = 0;
+
+       /*  
+       if (x%2==0)
+        {
+            result = 10;
+        }
+        else
+        {
+            result = 20;
+        }
+        */
+
+        result = x%2==0 ? 10 : 20;
+        System.out.println(result);
+    }    
+}
